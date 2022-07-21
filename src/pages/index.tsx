aa04318ba5da222a27ba96c3a98f641f9d54efa9
Bellergy RC <rc@bellergy.com>
2022-07-21 12:15:15 +0800

    Add layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,16 @@
-import Head from 'next/head';
-import Image from 'next/image';
-
-import styles from '@/styles/Home.module.css';
+import Layout from 'components/layout';
+import styles from 'components/Home/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>TypeScript starter for Next.js</title>
-        <meta
-          name="description"
-          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <Layout title="Home">
+      <main className="lockup">
+        <div className={styles.container}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+        </div>
       </main>
-
-    </div>
+    </Layout>
   );
 }
